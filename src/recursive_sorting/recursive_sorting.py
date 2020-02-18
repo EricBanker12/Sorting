459201db587c7884e38f4317a70658bc8c8f7104
ericbanker12 <ericbanker12@gmail.com>
2020-02-18 17:43:09 -0600

    wip implementation of timsort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -50,6 +50,55 @@ def merge_sort_in_place(arr, start=0, stop=None):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+import math
 
+def timsort( arr ):
+    # skip merge if short list
+    length = len(arr)
+    if length < 64:
+        return insertion_sort(arr)
+    # make short, sorted runs
+    run_length = math.ceil(length / 16)
+    runs = []
+    for i in range(0, len(arr), run_length):
+        runs.append(insertion_sort(arr[i:i + run_length]))
+    # merge runs
+    while len(runs) > 1:
+        runs[0] = merge(runs[0], runs.pop(1))
+        # print(runs)
+    return runs[0]
+
+def insertion_sort( arr ):
+    for i in range(1, len(arr)):
+        # look backwards
+        index = i
+        for j in range(i - 1, -1, -1):
+            # insert if less than
+            if arr[i] < arr[j]:
+                index = j
+        if index != i:
+            arr.insert(index, arr.pop(i))
     return arr
+
+import random
+import cProfile
+
+def test_tim():
+    test_arr = random.sample(range(10000),1024)
+    test_arr_2 = test_arr.copy()
+
+    # timsort
+    pr = cProfile.Profile()
+    pr.enable()
+    timsort(test_arr)
+    pr.disable()
+    pr.print_stats()
+
+    # merge sort
+    pr = cProfile.Profile()
+    pr.enable()
+    merge_sort(test_arr_2)
+    pr.disable()
+    pr.print_stats()
+
+test_tim()
